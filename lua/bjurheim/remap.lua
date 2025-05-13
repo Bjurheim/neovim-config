25946452821fadf5602881eb22da340bae36491d
Joel Bjurman <joel.bjurman@lm.se>
2025-05-13 10:49:54 +0200

    replicate 3 rows mouse scrolling with the keyboard

--- lua/bjurheim/remap.lua
+++ lua/bjurheim/remap.lua
@@ -58,3 +58,8 @@ end)
 
 vim.keymap.set("n", "<leader>D", vim.lsp.buf.definition, { noremap = true, silent = true })
 --vim.keymap.set("n", "<leader>r", vim.lsp.buf.references, { noremap = true, silent = true })
+
+-- scroll 3 lines down
+vim.keymap.set('n', '<C-j>', '3<C-E>', { noremap = true, silent = true, desc = "Scroll down 3 lines" })
+-- Scroll 3 lines up
+vim.keymap.set('n', '<C-k>', '3<C-Y>', { noremap = true, silent = true, desc = "Scroll up 3 lines" })
